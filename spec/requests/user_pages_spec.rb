50fcd96fffeb13d83622d83fd3bbc107eab9eaa8
Issam El Armi <issam.elarmi@gmail.com>
2012-03-25 23:10:33 +0000

    Finished chap 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,6 +26,14 @@ describe "User pages" do
 			it "should not create a user" do
 				expect { click_button "Create my account" }.not_to change(User, :count)
 			end
+
+			describe "error messages" do
+				before { click_button "Create my account" }
+
+				it { should have_selector('title', text: 'Sign up') }
+				it { should have_content('error') }
+			end
+
 		end
 
 		describe "with valid information" do
@@ -33,12 +41,21 @@ describe "User pages" do
 				fill_in "Name",				with: "Example User"
 				fill_in "Email", 			with: "user@example.com"
 				fill_in "Password", 		with: "foobar"
-				fill_in "confirmation",		with: "foobar"
+				fill_in "Confirmation",		with: "foobar"
 			end
 
 			it "should create a user" do
 				expect { click_button "Create my account"}.to change(User, :count).by(1)
 			end
+
+			describe "after saving the user" do
+				before { click_button "Create my account" }
+				let(:user) { User.find_by_email('user@example.com') }
+
+				it { should have_selector('title', text: user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+			end
+
 		end
 	
 	end
